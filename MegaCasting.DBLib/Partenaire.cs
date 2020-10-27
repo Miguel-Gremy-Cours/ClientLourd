711f7f387652abf9a3ace11b8b94795453ebf205
felixyangyicheng <felix.yang.yicheng@gmail.com>
2020-10-27 23:31:57 +0100

    ajouter menu tiroir

--- MegaCasting.DBLib/Partenaire.cs
+++ MegaCasting.DBLib/Partenaire.cs
@@ -14,7 +14,6 @@ namespace MegaCasting.DBLib
     
     public partial class Partenaire
     {
-        public int id { get; set; }
         public string Siret { get; set; }
         public string Adresse { get; set; }
         public int NumeroAdresse { get; set; }
@@ -23,5 +22,6 @@ namespace MegaCasting.DBLib
         public string Telephone { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public int Id { get; set; }
     }
 }
