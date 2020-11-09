ad81fc1308e09c8e3c99978a79b1a5f4411c075a
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-11-09 11:10:00 +0100

    Comment ViewModelContrat

--- MegaCasting.WPF/ViewModel/ViewModelTypeContrat.cs
+++ MegaCasting.WPF/ViewModel/ViewModelTypeContrat.cs
@@ -12,88 +12,82 @@ using System.Windows;
 
 namespace MegaCasting.WPF.ViewModel
 {
-    public class ViewModelTypeContrat:ViewModelBase
+    public class ViewModelTypeContrat : ViewModelBase
 
     {
-
-        
-
         #region Attributes
+        /// <summary>
+        /// Attribut contenant le TypeContrat de la vue
+        /// </summary>
         private TypeContrat _SelectedTypeContrat;
+        /// <summary>
+        /// Attribut contenant la liste des TypeContrats de la base de donnée
+        /// </summary>
         private ObservableCollection<TypeContrat> _TypeContrats;
+        /// <summary>
+        /// Attribut contenant la liste des Contrats de la base de donnée
+        /// </summary>
         private ObservableCollection<Contrat> _Contrats;
         #endregion
-
         #region Properties
+        /// <summary>
+        /// Contrats de la base de donnée
+        /// </summary>
         public ObservableCollection<Contrat> Contrats
         {
             get { return _Contrats; }
             set { _Contrats = value; }
         }
+        /// <summary>
+        /// TypeContrat de la vue
+        /// </summary>
         public TypeContrat SelectedTypeContrat
         {
             get { return _SelectedTypeContrat; }
             set { _SelectedTypeContrat = value; }
         }
+        /// <summary>
+        /// TypeContrat de la base de donnée
+        /// </summary>
         public ObservableCollection<TypeContrat> TypeContrats
         {
             get { return _TypeContrats; }
             set { _TypeContrats = value; }
         }
         #endregion
-
-
         #region constrcutor
-
-
-        public ViewModelTypeContrat(MegaCastingEntities entities):base(entities)
+        /// <summary>
+        /// Constructeur de la classe ViewModelTypeContrat
+        /// </summary>
+        /// <param name="entities"></param>
+        public ViewModelTypeContrat(MegaCastingEntities entities) : base(entities)
         {
-
-
+            // Initialisation de la liste des TypeContrats dans la base de donnée
             this.Entities.TypeContrats.ToList();
             this.TypeContrats = this.Entities.TypeContrats.Local;
+            // Initialisation de la liste des Contrats dans la base de donnée
             this.Entities.Contrats.ToList();
             this.Contrats = this.Entities.Contrats.Local;
-            //this.TypeContrats = new ObservableCollection<TypeContrat>();
-            //foreach (TypeContrat typeContrat in this.Entities.TypeContrats) 
-            //{
-            //    this.TypeContrats.Add(typeContrat);
-            //}
         }
         #endregion
-
         #region Method
-       /// <summary>
-       /// supprimer un type de contrat
-       /// </summary>
-
+        /// <summary>
+        /// supprimer un type de contrat
+        /// </summary>
         public void DeleteTypeContrats()
         {
-            // vérification de droit de suppression, aucune table liée
-            var typeEmpty = (from tp in TypeContrats
-                             join ctr in Contrats
-                             on tp.Id equals ctr.IdTypeContrat
-                             into x
-                             from ctr in x.DefaultIfEmpty()
-                             where ctr == null
-                             select tp
-                              );
-            //suppression d'élément
-
-
-            if (typeEmpty.Contains(SelectedTypeContrat))
+            // vérification de droit de suppression puis suppréssion de l'élément
+            try
             {
-                
-                    this.TypeContrats.Remove(SelectedTypeContrat);
-                    this.SaveChanges();
-               
+                this.TypeContrats.Remove(SelectedTypeContrat);
+                this.SaveChanges();
             }
-            else
+            catch (Exception)
             {
-                MessageBox.Show("Cette table ne peut être supprimée car il y a des données liées!", "ERROR");
+                MessageBox.Show("Impossible de supprimer cet élément", "OK");
             }
 
-            
+
         }
         #endregion
     }
