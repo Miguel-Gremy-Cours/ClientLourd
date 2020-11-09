7efbd18c84f980ea118a00ecf35859e7b05a691d
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-11-09 11:03:02 +0100

    Comment ViewModelPartenaires

--- MegaCasting.WPF/ViewModel/ViewModelPartenaires.cs
+++ MegaCasting.WPF/ViewModel/ViewModelPartenaires.cs
@@ -7,32 +7,34 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MegaCasting.WPF.ViewModel
 {
-   public  class ViewModelPartenaires:ViewModelBase
+    public class ViewModelPartenaires : ViewModelBase
     {
-
-
-
-
         #region Attributes
+        /// <summary>
+        /// Attribut contenant le Partenaire de la vue
+        /// </summary>
         private Partenaire _SelectedPartenaire;
+        /// <summary>
+        /// Attribut contenant la liste des Partenaires de la base de donnée
+        /// </summary>
         private ObservableCollection<Partenaire> _Partenaires;
- 
         #endregion
-
         #region Properties
-
-     
-
+        /// <summary>
+        /// Partenaires de la base de donnée
+        /// </summary>
         public ObservableCollection<Partenaire> Partenaires
         {
             get { return _Partenaires; }
             set { _Partenaires = value; }
         }
-
-
+        /// <summary>
+        /// Partenaire de la vue
+        /// </summary>
         public Partenaire SelectedPartenaire
         {
             get { return _SelectedPartenaire; }
@@ -40,29 +42,29 @@ namespace MegaCasting.WPF.ViewModel
         }
         #endregion
         #region Constrcutor
-        public ViewModelPartenaires(MegaCastingEntities entities):base(entities)
+        public ViewModelPartenaires(MegaCastingEntities entities) : base(entities)
         {
-
+            // Initialisation de la liste des Partenaires en base de donnée
             this.Entities.Partenaires.ToList();
             this.Partenaires = this.Entities.Partenaires.Local;
-           
-            
         }
         #endregion
-
-
         #region Method
         /// <summary>
         /// supprimer un partenaire
         /// </summary>
-
         public void DeletePartenaire()
         {
-            // vérification de droit de suppression, aucune table liée
-
-            //suppression d'élément
-            this.Partenaires.Remove(SelectedPartenaire);
-            this.SaveChanges();
+            // vérification de droit de suppression puis suppréssion des éléments
+            try
+            {
+                this.Partenaires.Remove(SelectedPartenaire);
+                this.SaveChanges();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Impossible de supprimer cet élément", "OK");
+            }
         }
         #endregion
     }
