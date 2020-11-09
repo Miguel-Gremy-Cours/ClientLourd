b7feda6fd779f86a7b3680eac18f3fb7736c435c
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-11-09 09:28:41 +0100

    Comment ViewModelCivilite

--- MegaCasting.WPF/ViewModel/ViewModelCivilite.cs
+++ MegaCasting.WPF/ViewModel/ViewModelCivilite.cs
@@ -10,61 +10,55 @@ using System.Windows;
 
 namespace MegaCasting.WPF.ViewModel
 {
-    public class ViewModelCivilite:ViewModelBase
+    public class ViewModelCivilite : ViewModelBase
     {
         #region Attributes
-
+        /// <summary>
+        /// Attribut contenant la Civilité selectionnée dans la vue
+        /// </summary>
         private Civilite _SelectedCivilite;
-
-
-
+        /// <summary>
+        /// Attribut contenant la liste des civilités de la base de donnée
+        /// </summary>
         private ObservableCollection<Civilite> _Civilites;
         #endregion
         #region Properties
-
-        public ObservableCollection<Civilite> Civilites
-        {
-            get { return _Civilites; }
-            set { _Civilites = value; }
-        }
-
+        /// <summary>
+        /// Civilité selectionnée dans la vue
+        /// </summary>
         public Civilite SelectedCivilite
         {
             get { return _SelectedCivilite; }
             set { _SelectedCivilite = value; }
         }
-        #endregion
-
-        #region Constructor
-
-        public ViewModelCivilite(MegaCastingEntities entities) : base(entities)
+        /// <summary>
+        /// Civilités de la base de donnée
+        /// </summary>
+        public ObservableCollection<Civilite> Civilites
         {
-
-            this.Entities.Civilites.ToList();
-            this.Civilites = this.Entities.Civilites.Local;
-
-           
+            get { return _Civilites; }
+            set { _Civilites = value; }
         }
         #endregion
-
-
-        #region Method
-
-
-
+        #region Constructor
         /// <summary>
-        /// supprimer une civilité
+        /// Constructeur de la classe 'ViewModelCivilite'
         /// </summary>
-
+        /// <param name="entities"></param>
+        public ViewModelCivilite(MegaCastingEntities entities) : base(entities)
+        {
+            // Initialisation de la liste des Civilités dans la variable
+            this.Entities.Civilites.ToList();
+            this.Civilites = this.Entities.Civilites.Local;
+        }
+        #endregion
+        #region Method
+        /// <summary>
+        /// Supprimer une civilité
+        /// </summary>
         public void DeleteCivilite()
         {
-            // vérification de droit de suppression,  table civilité liée à Employes, Internautes
-
-            //suppression d'élément
-
-     
-
-
+            // Vérification de droit de suppression puis suppréssion d'élément
             try
             {
                 this.Civilites.Remove(SelectedCivilite);
@@ -72,12 +66,9 @@ namespace MegaCasting.WPF.ViewModel
             }
             catch (Exception)
             {
-
-                MessageBox.Show("Cette table ne peut être supprimée car il y a des données liées!", "ERROR");
+                MessageBox.Show("Impossibe de supprimer cet élément", "OK");
             }
         }
-
-
         #endregion
     }
 }
